12274a1942bf145c6d675f5b1154c4e4d0f09e3e
vsmithbe-git <98296635+vsmithbe-git@users.noreply.github.com>
2022-05-03 13:57:21 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
+
+import config from './utils/config'
+import logger from './utils/logger'
+import errors from './utils/errors'
+
+import router from './routes'
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(logger.middleware)
+app.use(helmet())
+app.use(
+  cors({
+    origin: config.origin,
+  }),
+)
+
+app.use(router)
+
+app.use(errors.notFound)
+app.use(errors.errorHandler)
+
+app.listen(config.port)
